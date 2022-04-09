b9a2924dd6a6671ae51aa3a7ff6c0ea5dcbe3203
Michael Obubelebra Amachree <70345027+Michael-Obele@users.noreply.github.com>
2022-04-09 16:23:39 +0100

    Start up

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
+ const [value,setValue] = useState('');
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1 className='text-center'>Hello World!</h1>
+      {/* You can use bg-dark for dark theme */}
+      <div class='card converter text-dark bg-light mb-3'>
+        <div class='card-body'>
+          <h4 class='card-title text-center'>
+            Enter Roman Numerial or Number:
+          </h4>
+          <div class='form-floating'>
+            <input
+              type='text'
+              class='form-control'
+              id='textInput'
+              placeholder='Type Here'
+              onChange={(e) => setValue(e.target.value)}
+            />
+            <label for='textInput'>Type Here</label>
+          </div>
+        </div>
+        <div className='card-body content'>
+          <h3>{value}</h3>
+        </div>
+      </div>
     </div>
   );
 }
